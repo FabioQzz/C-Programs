61999a56a3a8245832f41126885b3bb8d228bb6a
FabioQzz <92087170+FabioQzz@users.noreply.github.com>
2023-11-11 17:00:05 +0100

    Input implemented

--- numeric_integration/rectangular_integration.c
+++ numeric_integration/rectangular_integration.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define max 10
+float valutaHorner(float P[11],float x);
+
+void main(){
+    int coeff[max], i;
+    int grade, a, b, M;
+
+    printf("Insert the grade: ");
+    scanf(" %d", &grade);
+    if(grade <= max){
+        printf("Insert the %d+1 coeffcients:\n", grade);
+        for(i=0; i<=grade; i++){
+            scanf(" %d", &coeff[i]);            
+        }
+    }else{
+        printf("Grade not valid\n");
+        return;
+    }
+
+    printf("Insert the extremes of integration: ");
+    scanf("%d %d\n", &a, &b);
+    printf("Insert M: ");
+    scanf("%d\n", &M);
+
+
+
+
+
+}
