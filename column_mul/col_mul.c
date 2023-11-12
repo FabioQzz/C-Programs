766aa5b82fe81fb8a9f6e4704bd8f26facfbe04f
FabioQzz <92087170+FabioQzz@users.noreply.github.com>
2023-11-12 18:45:19 +0100

    final update

--- column_mul/col_mul.c
+++ column_mul/col_mul.c
@@ -16,16 +16,24 @@ int main(){
 }
 
 void mul(int *v1, int *v2, int n, int *v3){
-    int i, j, pos;        //pos is the index of v3: it starts at 2*n+1
-    int carry, shift;     //carry of the mul of 2 digits, shift of the res
+    int i, j, pos, sum;        //pos is the index of v3: it starts at 2*n+1
+    int carry=0, shift;     //carry of the mul of 2 digits, shift of the res
 
-    for(i=3; i>0; i-- ){  //external loop iterates on v2 multiplicator
-        
-        for(j=3; j>0; j++){ //internal loop iterates on v1 multiplicand
-            v3[pos] = (v2[i] * v1[j]) + carry;
-            carry = v3[pos] % 10;
+    for(i=n-1, shift=0; i>=0; i--, shift++ ){  //external loop iterates on v2 multiplicator
+        carry= 0;
+        for(j= n-1, pos= 2*n-1; j>=0; j--, pos--){ //internal loop iterates on v1 multiplicand
+            sum = v3[pos-shift] + (v2[i] * v1[j]) + carry;
+            v3[pos-shift] = sum % 10;
+            carry = sum / 10;
+            printf("carry: %d\n", carry);
+        }
+        if(carry!=0){
+            v3[pos-shift]= carry;
         }
     }
 
+    printf("Product = ");
+    for(j=0; j<2*n; j++)
+        printf("%d", v3[j]);
 
 }
